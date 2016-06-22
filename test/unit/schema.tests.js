48a6ba89cea81eb485e975977fb8cfda5f5d7425
Jarrett Cruger <jcrugzz@gmail.com>
2016-06-21 23:27:18 -0400

    [minor] style fixes

--- test/unit/schema.tests.js
+++ test/unit/schema.tests.js
@@ -4,16 +4,16 @@ var assume            = require('assume'),
     datastarTestTools = require('datastar-test-tools'),
     joi               = require('joi-of-cql'),
     schemas           = require('../fixtures/schemas'),
-    Schema            = require('../../lib/schema'),
-    helpers           = datastarTestTools.helpers;
+    Schema            = require('../../lib/schema');
 
-var debug = helpers.debug,
-    cql   = joi.cql;
+var helpers = datastarTestTools.helpers,
+    debug   = helpers.debug,
+    cql     = joi.cql;
 
-describe('Schema (unit)', function() {
+describe('Schema (unit)', function () {
   var schema;
 
-  it('should create a schema', function() {
+  it('should create a schema', function () {
     schema = new Schema('artist', schemas.artist);
     debug(schema);
 
@@ -22,7 +22,7 @@ describe('Schema (unit)', function() {
     assume(schema.primaryKeys()).to.deep.equal(['artist_id']);
   });
 
-  it('should throw an error when given an invalid schema', function() {
+  it('should throw an error when given an invalid schema', function () {
     var invalid = joi.object({
       id: cql.uuid()
     });
@@ -34,7 +34,7 @@ describe('Schema (unit)', function() {
     assume(init).throws(/must define a partitionKey/);
   });
 
-  it('should throw an error when given an invalid name for the schema', function() {
+  it('should throw an error when given an invalid name for the schema', function () {
     function init() {
       Schema('has-dashes');
     }
@@ -42,7 +42,7 @@ describe('Schema (unit)', function() {
     assume(init).throws('Invalid character in schema name');
   });
 
-  it('should transform the schema (snakecase and aliases)', function() {
+  it('should transform the schema (snakecase and aliases)', function () {
     var entity = [
       'createDate'
     ];
@@ -51,7 +51,7 @@ describe('Schema (unit)', function() {
     debug(fields);
   });
 
-  it('should validate the schema', function() {
+  it('should validate the schema', function () {
     var entity = {
       name: 'foo',
       createDate: new Date(),
@@ -63,7 +63,7 @@ describe('Schema (unit)', function() {
     debug(valid);
   });
 
-  it('should allow for null values', function() {
+  it('should allow for null values', function () {
     var entity = {
       name: 'whocares',
       createDate: new Date(),
